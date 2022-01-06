efecbae76b316ef361e797e086a7ae366ed7f818
Markun1975 <choco_9031@yahoo.co.jp>
2022-01-06 23:42:36 +0900

    スワイプ実装

--- TestTableView/ViewController.swift
+++ TestTableView/ViewController.swift
@@ -15,9 +15,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
             super.viewDidLoad()
-            // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         tableView.register(UINib(nibName: "SampleTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomCell")
-        tableView.isEditing = true
+//        tableView.isEditing = false
         tableView.allowsSelectionDuringEditing = true
         }
     
@@ -37,6 +39,50 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.view.frame.height/10
     }
     
+    /**
+     この実装は左スワイプ削除実装
+     **/
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+            // 先にデータを削除しないと、エラーが発生します。
+//            self.tableData.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteButton = UITableViewRowAction(style: .normal, title: "削除", handler: { _, indexPath in
+            tableView.dataSource?.tableView?(tableView, commit: .delete, forRowAt: indexPath)
+        })
+        deleteButton.backgroundColor = UIColor.blue
+        return [deleteButton]
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?{
+        // 編集処理
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { (action, view, completionHandler) in
+            // 編集処理を記述
+            print("Editがタップされた")
+
+            // 実行結果に関わらず記述
+            completionHandler(true)
+        }
+        // 削除処理
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
+            //削除処理を記述
+            print("Deleteがタップされた")
+
+            // 実行結果に関わらず記述
+            completionHandler(true)
+        }
+        // 定義したアクションをセット
+        return UISwipeActionsConfiguration(actions: [deleteAction, editAction])
+}
+    
+    /**
+     この実装は並び替え実装
+     **/
+    
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
             return true
         }
@@ -49,10 +95,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return .none
     }
     
-    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-    
+//    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+//        return false
+//    }
+//
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.subviews.forEach { (subview) in
             if String(describing: Swift.type(of: subview.self)) == "UITableViewCellReorderControl" {
